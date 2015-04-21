1b03b32f6f958650ca0d55cc730cb8b7baccc8f6
IlinAleksey <alekseyilinmusic@gmail.com>
2015-04-21 12:40:28 +0300

    Implemented OracleBuilder

--- SimpleLR1Analyser/Oracle.h
+++ SimpleLR1Analyser/Oracle.h
@@ -12,6 +12,8 @@ struct OracleAction
 	OracleActionType type;
 	int value;
 	
+	
+
 	bool is_empty() { if (type == OracleActionType::error) return true; else return false; }
 
 	friend std::ostream& operator<< (std::ostream& out, OracleAction oracle_action)
